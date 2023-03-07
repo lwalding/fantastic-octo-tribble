7014f39194aa5a29543e8cce4ab94c522486549e
Luke Walding <luke.w@me.com>
2023-03-07 17:05:07 +1100

    Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), prefetch(), partytown(), image(), mdx(), alpinejs()]
+});
